f704f1cea184b0f39d50434d9a9bbe969e099869
Sayem Ahmed <haphazardsayem@gmail.com>
2025-12-04 23:23:33 +0600

    AlapOn-01_feat : Creating the entities and the relationships

---
+++ AlapOn/src/main/java/com/haphazard/AlapOn/chat/ChatConstants.java
@@ -0,0 +1,8 @@
+package com.haphazard.AlapOn.chat;
+
+public class ChatConstants {
+  private ChatConstants() {}
+
+  public static final String FIND_CHAT_BY_SENDER_ID = "Chat.findChatBySenderId";
+  public static final String FIND_CHAT_BY_SENDER_ID_AND_RECEIVER_ID = "Chat.findChatBySenderIdAndReceiverId";
+}
---
+++ AlapOn/src/main/java/com/haphazard/AlapOn/message/Message.java
@@ -0,0 +1,59 @@
+package com.haphazard.AlapOn.message;
+
+import static com.haphazard.AlapOn.message.MessageConstants.FIND_MESSAGES_BY_CHAT_ID;
+import static com.haphazard.AlapOn.message.MessageConstants.SET_MESSAGES_TO_SEEN_BY_CHAT;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import com.haphazard.AlapOn.chat.Chat;
+import com.haphazard.AlapOn.common.BaseAuditingEntity;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "message")
+@NamedQuery(name = FIND_MESSAGES_BY_CHAT_ID, query = "SELECT m FROM Message m WHERE m.chat.id = :chatId ORDER BY m.createdDate")
+@NamedQuery(name = SET_MESSAGES_TO_SEEN_BY_CHAT, query = "UPDATE Message SET state = :newState WHERE chat.id = :chatId")
+public class Message extends BaseAuditingEntity {
+
+  @Id
+  @SequenceGenerator(name = "msg_seq", sequenceName = "msg_seq", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "msg_seq")
+  private Long id;
+
+  @Column(columnDefinition = "TEXT")
+  private String content;
+
+  @Enumerated(EnumType.STRING)
+  private MessageState state;
+
+  @Enumerated(EnumType.STRING)
+  private MessageType type;
+
+  @ManyToOne
+  @JoinColumn(name = "chat_id")
+  private Chat chat;
+
+  @Column(name = "sender_id", nullable = false)
+  private String senderId;
+
+  @Column(name = "receiver_id", nullable = false)
+  private String receiverId;
+}
